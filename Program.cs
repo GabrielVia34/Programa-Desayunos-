95c610514823886fe3588614f3ebca29f2f09abe
GabrielVia34 <andregvc1@gmail.com>
2024-11-12 22:02:26 -0500

    subida program.cs

--- Program.cs
+++ Program.cs
@@ -1,15 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Program
+namespace DesayunoApp
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            
+            ProgramaDesayuno programa = new ProgramaDesayuno();
+
+            while (true)
+            {
+               
+                Console.Clear();
+                Console.WriteLine("=================================");
+                Console.WriteLine("      MENU DE DESAYUNOS");
+                Console.WriteLine("=================================");
+                Console.WriteLine("1) Crear desayuno");
+                Console.WriteLine("2) Listar desayunos");
+                Console.WriteLine("3) Seleccionar desayuno");
+                Console.WriteLine("4) Salir");
+                Console.WriteLine("=================================");
+                Console.Write("Seleccione una opción: ");
+
+              
+                string opcion = Console.ReadLine();
+
+               
+                switch (opcion)
+                {
+                    case "1":
+                       
+                        Console.Write("Ingrese el nombre del cliente: ");
+                        string cliente = Console.ReadLine();
+                        programa.CrearDesayuno(cliente);
+                        break;
+
+                    case "2":
+                        
+                        programa.ListarDesayunos();
+                        break;
+
+                    case "3":
+                       
+                        Console.Write("Ingrese el nombre del desayuno: ");
+                        string nombre = Console.ReadLine();
+                        Console.Write("Ingrese el día para el desayuno: ");
+                        string dia = Console.ReadLine();
+                        programa.SeleccionarDesayuno(nombre, dia);
+                        break;
+
+                    case "4":
+                       
+                        Console.WriteLine("¡Gracias por usar el programa! Saliendo...");
+                        return;
+
+                    default:
+                     
+                        Console.WriteLine("[!] Opcion no valida, por favor intente nuevamente.");
+                        break;
+                }
+
+
+                Console.WriteLine("Presione cualquier tecla para continuar...");
+                Console.ReadKey();
+            }
         }
     }
 }
+
